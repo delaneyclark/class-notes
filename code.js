4f20f1841246e528192a919fb9fe0981c0bfa953
delaneyclark <delaneyclark6@gmail.com>
2015-04-06 19:58:15 -0400

    Console Clear
    console log the current weather observation

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	$.getJSON("http://cooper-union-weather-proxy.herokuapp.com/weather/NY/Manhattan", function(response) {
 
 		var weather = response.current_observation.feelslike_f;
+		
+		console.log(response.current_observation.weather.toLowerCase());
 
 		if ((weather < 50) && (weather > 32) || (weather < 0)) {
 			console.log("it is ok")
