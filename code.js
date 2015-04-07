ea09f303f226e95eb44cc3b2f5682d23c6db7bf9
delaneyclark <delaneyclark6@gmail.com>
2015-04-06 20:57:54 -0400

    console log breakdown

--- code.js
+++ code.js
@@ -1,33 +1,21 @@
 // logic examples 
 $(document).ready(function(){
-	$.getJSON("http://cooper-union-weather-proxy.herokuapp.com/weather/NY/Manhattan", function(response) {
 
-		var weather = response.current_observation.feelslike_f;
-		var condition = response.current_observation.weather.toLowerCase();
 
-		if (
-			(condition == "clear") || (condition == "snow") || (condition == "fog")) {
-				console.log("moist airs");
-		}
-
-		var rainyConditions = ["rain", "snow", "fog"];
-
-		if((rainyConditions.indexOf(condition) >= 0) && (weather > 40)){
-			console.log("its rainy and over fourty degrees")
-		}
-
-		if ((weather < 50) && (weather > 32) || (weather < 0)) {
-			console.log("it is ok")
-		} else {
-			console.log("its ah")
-		}	
-
-		var hello = function(name) {
-			console.log("hey hi" + name)
+		var hello = function(name, weather) {
+			console.log("hey hi " + name + " isn't the " + weather + " nice?")
 		};
 
-		hello("nate");
-		hello("dlane");
-	});
+		hello("nate", "rain");
 
-})
+		//.something accesses the key 
+
+		var output = function(information) {
+			//do something to output variable 'information'
+		}
+
+		var console = {
+			"log": outpuf
+		}
+		console.log("aidfnaiso")
+	});
