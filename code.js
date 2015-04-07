f92ea179b8dd113a0d7a38067da368b2d9743736
delaneyclark <delaneyclark6@gmail.com>
2015-04-06 21:39:23 -0400

    Self Closing tags
    self closing tags and parameters and index Of

--- code.js
+++ code.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
 
 
 	var soyIt = function(tag, statement) {
-		if (tag === "img") {
+
+		var selfClosingTags = ["img", "br"];
+
+		if (selfClosingTags.indexOf(tag) >= 0) {
 			var output= "<" + tag + " src=" + statement + " />";
 		} else {
 			var openTag = "<" + tag + ">";
@@ -17,5 +20,6 @@ $(document).ready(function(){
 	soyIt("h1", "this is 1");
 	soyIt("h2", "this is 2");
 	soyIt("img", "http://farm8.staticflickr.com/7341/9721390539_97c6cc53e1.jpg");
+	soyIt("br")
 
 });
