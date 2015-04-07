7dad81814cf1e1d26ba36b78f24505e9f679b9e8
delaneyclark <delaneyclark6@gmail.com>
2015-04-06 21:35:00 -0400

    Function Image
    Bring an image into a function, yay a

--- code.js
+++ code.js
@@ -4,15 +4,18 @@ $(document).ready(function(){
 
 
 	var soyIt = function(tag, statement) {
-		var openTag = "<" + tag + ">";
-		var closeTag = "</" + tag + ">";
-		var output = openTag + statement + closeTag;
-
-		$("body").append(output)
-
+		if (tag === "img") {
+			$("body").append("<img src=" + statement + "/>")
+		} else {
+			var openTag = "<" + tag + ">";
+			var closeTag = "</" + tag + ">";
+			var output = openTag + statement + closeTag;
+		}	
+		$("body").append(output);
 	}
 
-	soyIt("h1", "this is 1"),
-	soyIt("h2", "this is 2")
+	soyIt("h1", "this is 1");
+	soyIt("h2", "this is 2");
+	soyIt("img", "http://farm8.staticflickr.com/7341/9721390539_97c6cc53e1.jpg");
 
 });
