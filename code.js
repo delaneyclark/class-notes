06047d29f7b8df10f54083fee1c528dfe366efed
delaneyclark <delaneyclark6@gmail.com>
2015-04-06 20:03:52 -0400

    other logics
    unnecessary logic that makes it complicated, but for learning (using &&)

--- code.js
+++ code.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 	$.getJSON("http://cooper-union-weather-proxy.herokuapp.com/weather/NY/Manhattan", function(response) {
 
 		var weather = response.current_observation.feelslike_f;
-
 		var condition = response.current_observation.weather.toLowerCase();
 
 		if (
 			(condition == "clear") || (condition == "snow") || (condition == "fog")) {
-				console.log("moist airs")
+				console.log("moist airs");
+		}
+
+		var rainyConditions = ["rain", "snow", "fog"];
+
+		if((rainyConditions.indexOf(condition) >= 0) && (weather > 40)){
+			console.log("its rainy and over fourty degrees")
 		}
 
 		if ((weather < 50) && (weather > 32) || (weather < 0)) {
